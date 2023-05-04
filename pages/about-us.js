9a01a478f7aa84f2b18e4fae6dec0447f7ddeaf2
khattarmanali <manalikhattar1@gmail.com>
2023-05-04 08:44:52 +0530

    added commponents

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,22 +1,22 @@
 // import React from 'react'
-import { DefaultSeo, LogoJsonLd } from 'next-seo'
-import { FadeRight } from 'src/components/animate'
-import GlobalSEO, { SITE_LOGO, SITE_URL } from 'src/data/next-seo.data'
-import { MainAboutPage } from '../components/aboutAs'
+import { MainAboutPage } from "components /aboutAs";
+import { DefaultSeo, LogoJsonLd } from "next-seo";
+import { FadeRight } from "../components /animate";
+import GlobalSEO, { SITE_LOGO, SITE_URL } from "../data/next-seo.data";
 
 const AboutUs = () => {
   return (
     <>
-      <DefaultSeo {...GlobalSEO.global} {...GlobalSEO['/about-us']} />
+      <DefaultSeo {...GlobalSEO.global} {...GlobalSEO["/about-us"]} />
       <LogoJsonLd logo={SITE_LOGO} url={SITE_URL} />
-      <FadeRight durationTime={'1s'}>
+      <FadeRight durationTime={"1s"}>
         <MainAboutPage />
       </FadeRight>
     </>
-  )
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
 
 // export async function getStaticProps() {
 //   const data = '/about-us'
