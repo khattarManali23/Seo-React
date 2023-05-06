c96ad156c365e658d10b188534763f8968d68c24
khattarmanali <manalikhattar1@gmail.com>
2023-05-06 13:24:59 +0530

    added head

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,14 +1,36 @@
 // import React from 'react'
 import { MainAboutPage } from "components/aboutAs";
 import { DefaultSeo, LogoJsonLd } from "next-seo";
+import Head from "next/head";
 import { FadeRight } from "../components/animate";
 import GlobalSEO, { SITE_LOGO, SITE_URL } from "../data/next-seo.data";
 
 const AboutUs = () => {
   return (
     <>
-      <DefaultSeo {...GlobalSEO.global} {...GlobalSEO["/about-us"]} />
-      <LogoJsonLd logo={SITE_LOGO} url={SITE_URL} />
+      <Head>
+        <title>My Page Title</title>
+        <meta name="description" content="This is a description of my page" />
+        <meta name="keywords" content="keyword1, keyword2, keyword3" />
+        <meta name="author" content="My Name" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="canonical" href="https://example.com/my-page" />
+        <meta property="og:title" content="My Page Title" />
+        <meta
+          property="og:description"
+          content="This is a description of my page"
+        />
+        <meta property="og:image" content="https://example.com/my-image.jpg" />
+        <meta property="og:url" content="https://example.com/my-page" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="My Page Title" />
+        <meta
+          name="twitter:description"
+          content="This is a description of my page"
+        />
+        <meta name="twitter:image" content="https://example.com/my-image.jpg" />
+      </Head>
       <FadeRight durationTime={"1s"}>
         <MainAboutPage />
       </FadeRight>
